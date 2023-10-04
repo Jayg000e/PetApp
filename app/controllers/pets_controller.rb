18cb3effecee1e0a1d78532945d5df781e264bf4
Jayg000e <jg4692@columbia.edu>
2023-10-04 11:56:57 -0700

    getbyuserId controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,7 +10,17 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     render json: @pet
   end
+  def by_user
+    # Retrieve pets belonging to the user with the provided :userid
+    user_id = params[:userid]
+    pets = Pet.where(user_id: user_id) # Assuming you have a Pet model
 
+    if pets.empty?
+      render json: { error: "No pets found for user with ID #{user_id}" }, status: :not_found
+    else
+      render json: pets, status: :ok
+    end
+  end
 
   def new
     @pet = Pet.new
