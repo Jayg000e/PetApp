b78fc74753b71ec8a8edc4e6b951f4c78fdc0d22
yuhsin-huang <97138478+yuhsin-huang@users.noreply.github.com>
2023-10-04 16:38:35 -0400

    Create User Controller
    Create user controller for CRUD operations

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # get '/pets', to: 'pets#index'
-  resources :pets
 
+  resources :pets
+  resources :users
+  # get '/example_action', to: 'controller_name#example_action'
+  get '/pets/user/:userid', to: 'pets#by_user'
+  
 end
