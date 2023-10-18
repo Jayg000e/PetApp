99da700d07bd47e930b2e8aa242c29175975c3bc
1640675651 <1640675651@qq.com>
2023-10-18 20:56:01 +0000

    add cucumber for marketplace and pet page

--- spec/requests/renderer_spec.rb
+++ spec/requests/renderer_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Renderers", type: :request do
-  describe "GET /allpets" do
+  describe "GET /render/pets" do
     it "returns http success" do
-      get "/renderer/allpets"
+      get "/render/pets"
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe "GET /render/pets/:id" do
+    it "returns http success" do
+      get "/render/pets/1"
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe "GET /render/pets/:petid/activities" do
+    it "returns http success" do
+      get "/render/pets/1/activities"
       expect(response).to have_http_status(:success)
     end
   end
