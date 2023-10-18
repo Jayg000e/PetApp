43ad280e25929348bd5f5308993eca747bc71c8c
Jayg000e <jg4692@columbia.edu>
2023-10-17 17:43:29 -0700

    conv setup

--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -1,38 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe PetsController, type: :controller do
-  describe "GET #index" do
-    it "returns a JSON response with a list of pets" do
-      # Create test data by manually creating Pet records
-      pet1 = Pet.create(name: "Dextero", pet_type: "Dog", birthdate: "2019-05-10", breed: "Golden Retriever")
-      # Add more pet records as needed
+  describe 'GET #index' do
+    let!(:pets) { create_list(:pet, 3) } # Creates 3 pets
 
-      # Send a GET request to the index action
-      get :index, format: :json
+    it 'returns all pets' do
+      get :index
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body).size).to eq(3)
+    end
+  end
 
-      # Expect a successful response (HTTP status 200)
-      expect(response).to have_http_status(:success)
+  describe 'GET #show' do
+    let(:pet) { create(:pet) }
 
-      # Parse the JSON response
-      json_response = JSON.parse(response.body)
+    it 'returns the requested pet' do
+      get :show, params: { id: pet.id }
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)["id"]).to eq(pet.id)
+    end
+  end
 
-      # Convert the birthdate attribute in the expected hash to a string
-      expected_birthdate = pet1.birthdate.strftime("%Y-%m-%d")
+  describe 'GET #by_user' do
+    let(:user) { create(:user) }
+    let!(:pets) { create_list(:pet, 2, user: user) }
 
-      # Modify the expectation to use the string representation of birthdate
-      # Modify the expectation to access the first element of the json_response array
-      expect(json_response.first).to include(
-        {
-          "id" => pet1.id,
-          "name" => pet1.name,
-          "pet_type" => pet1.pet_type,
-          "birthdate" => expected_birthdate,  # Use the string representation
-          "breed" => pet1.breed,
-        }
-        # Add similar hashes for other pet records
-        # Add other attributes as needed
-      )
+    context 'when user has pets' do
+      it 'returns pets of the user' do
+        get :by_user, params: { userid: user.id }
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body).size).to eq(2)
+      end
+    end
 
+    context 'when user has no pets' do
+      it 'returns an error' do
+        get :by_user, params: { userid: 999 } # A non-existent user ID
+        expect(response).to have_http_status(:not_found)
+        expect(JSON.parse(response.body)["error"]).to eq("No pets found for user with ID 999")
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid parameters' do
+      let(:valid_params) { attributes_for(:pet) } # Generates a hash of pet attributes
+
+      it 'creates a new pet' do
+        expect {
+          post :create, params: { pet: valid_params }
+        }.to change(Pet, :count).by(1)
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'with invalid parameters' do
+      let(:invalid_params) { attributes_for(:pet, name: nil) } # Generates invalid pet attributes
+
+      it 'does not create a pet and returns an error' do
+        expect {
+          post :create, params: { pet: invalid_params }
+        }.not_to change(Pet, :count)
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
     end
   end
 end
