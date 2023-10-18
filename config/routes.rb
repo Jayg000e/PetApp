4e949f4021c29a5198f869b6844636a842abf57c
Yunjia Wang <yunjiw1@gmail.com>
2023-10-18 17:29:14 -0400

    view: get pets by user id

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   resources :activities
   resources :users
   get '/pets/user/:userid', to: 'pets#by_user'
+  get '/activities/pet/:petid', to: 'activities#by_pet'
 
   # frontend route
   get '/render/pets', to: 'renderer#allpets'
   get '/render/pets/:id', to: 'renderer#getPetById'
   get '/render/pets/:petid/activities', to: 'renderer#getActivities'
-  get '/activities/pet/:petid', to: 'activities#by_pet'
+  get '/render/users/:userid/pets', to: 'renderer#getPetsByUserId'
+  
 end
