aeeac650f86384b0546b7bf72f4e55cb01a77438
Jayg000e <jg4692@columbia.edu>
2023-10-24 00:10:21 -0700

    psql set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_18_184852) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_175331) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: :cascade do |t|
     t.string "content"
-    t.integer "pet_id"
+    t.bigint "pet_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["pet_id"], name: "index_activities_on_pet_id"
@@ -28,7 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_18_184852) do
     t.datetime "updated_at", null: false
     t.boolean "onsale"
     t.float "price"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
